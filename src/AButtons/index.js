76042f4924e2088932cad3e39d366ee008d9cba1
陳邦菁 <bengjing@gmail.com>
2016-05-31 08:41:40 +0800

    Change directory to Buttons

---
+++ src/AButtons/index.js
@@ -0,0 +1,7 @@
+export AFloatingActionButton from './AFloatingActionButton.react';
+export ARaisedButton from './ARaisedButton.react';
+export AFlatButton from './AFlatButton.react';
+export AnIconToggle from './AnIconToggle.react';
+export AnIconButton from './AnIconButton.react';
+
+export default from './ARaisedButton.react';
--- src/main.js
+++ src/main.js
@@ -6,10 +6,6 @@ require('./aframe-components/aframe-ripple');
 require('./aframe-components/aframe-shadow');
 require('./aframe-components/aframe-svg');
 
-export AFloatingActionButton from './components/AFloatingActionButton.react';
-export ARaisedButton from './components/ARaisedButton.react';
-export AFlatButton from './components/AFlatButton.react';
-export AnIconToggle from './components/AnIconToggle.react';
-export AnIconButton from './components/AnIconButton.react';
+export AButtons from './AButtons';
 
 export ACursor from './components/ACursor.react';
